34dcf4445b00d8a33cb1e2b540521ef630b91444
telemtaev <r.telemtaev@gmail.com>
2015-06-07 20:53:52 +0400

    123

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php 
-define("HOST","localhost");
-define("USER","trdesign_admin");
-define("PASSWORD","lWU&cz5Pz5[;");
-define("DB","trdesign_newcms");
-?>
--- db_connect.php
+++
@@ -1,7 +0,0 @@
-<?php 
-		$lnk = mysql_connect(HOST,USER,PASSWORD);
-		if(!mysql_select_db(DB,$lnk)) {
-			exit("Нет такой базы данных ".mysql_error());
-		}
-		mysql_query("SET NAMES 'UTF8'");
-?>
